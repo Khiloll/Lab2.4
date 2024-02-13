c0e1cd5961f68f5333bf79b6c1b09595a8180249
Hilol <97358655+Khiloll@users.noreply.github.com>
2024-02-13 16:09:00 +0300

    Update individualnoe1.py

--- код/individualnoe1.py
+++ код/individualnoe1.py
@@ -1,3 +1,7 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 def main():
     A = [5, 3, 6, -2, 9, 12, -7, 15, 6, 8]  # Пример списка A из 10 элементов
     positive_multiples_of_3 = [num for num in A if num > 0 and num % 3 == 0]  # Выбираем положительные элементы, кратные 3
